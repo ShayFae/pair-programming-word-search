52382c3fd914f49e50e27c6d66574ce4603cf7a6
ShayFae <ifrahnoor108@gmail.com>
2022-01-20 01:59:33 +0000

    Eslint

--- wordsearch.js
+++ wordsearch.js
@@ -1,19 +1,19 @@
 const { transpose } = require('./transpose');
-const wordSearch = (letters, word) => { 
-    const crazy = (matrix) => {
-        const join = matrix.map(ls => ls.join(''));
-        for (const h of join) {
-            if (h.includes(word)) return true;
-        };
+const wordSearch = (letters, word) => {
+  const crazy = (matrix) => {
+    const join = matrix.map(ls => ls.join(''));
+    for (const h of join) {
+      if (h.includes(word)) return true;
     }
-    let callback1 = crazy(letters); 
-    if (callback1) {
-        return callback1;
-    }
-    let callback2 = crazy(transpose(letters));
-    if (callback2) {
-        return callback2;
-    }
-    return false;
- };
+  };
+  let callback1 = crazy(letters);
+  if (callback1) {
+    return callback1;
+  }
+  let callback2 = crazy(transpose(letters));
+  if (callback2) {
+    return callback2;
+  }
+  return false;
+};
 module.exports = wordSearch;
